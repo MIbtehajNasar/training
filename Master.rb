22ca58b5973c7399021ff4ade9cfd43eb4e3acff
MIbtehajNasar <ibtehajnasar@yahoo.com>
2021-01-20 18:16:28 +0500

    Flatten an array code

--- Master.rb
+++ Master.rb
@@ -1,6 +1,6 @@
-require 'csv'
+# require 'csv'
 
-=begin
+
 
 program to flatten an array
 
@@ -15,7 +15,7 @@ test = Array['a', 1, ['b', 2], 'c', [3, ['d', 4]]]
 flatten_array test
 p @out
 
-
+=begin
 # Program to get the most frequent element from an array
 
 def random_array(size)
@@ -39,44 +39,44 @@ end
 check_frequency 5
 =end
 
-def generate_password(total_length, numbers, alphabets, spchar)
-  return puts 'yolo' unless total_length == (numbers + alphabets + spchar)
+# def generate_password(total_length, numbers, alphabets, spchar)
+#   return puts 'yolo' unless total_length == (numbers + alphabets + spchar)
 
-  n = Array.new(numbers){ rand 9 }.join
-  charset = Array('A'..'Z') + Array('a'..'z')
-  a = Array.new(alphabets) { charset.sample }.join
-  sp = Array('!'..'/') + Array(':'..'?')
-  s = Array.new(spchar){ sp.sample }.join
-  a << n << s
-  r = a.split('').shuffle.join
-  p r
-end
+#   n = Array.new(numbers){ rand 9 }.join
+#   charset = Array('A'..'Z') + Array('a'..'z')
+#   a = Array.new(alphabets) { charset.sample }.join
+#   sp = Array('!'..'/') + Array(':'..'?')
+#   s = Array.new(spchar){ sp.sample }.join
+#   a << n << s
+#   r = a.split('').shuffle.join
+#   p r
+# end
 
-def save_password(filename)
-  CSV.open(filename, "a") do |csv|
-    (0..9).each do
-      pass = generate_password(10,6,2,2)
-      csv << [pass]
-    end
-  end
-end
+# def save_password(filename)
+#   CSV.open(filename, "a") do |csv|
+#     (0..9).each do
+#       pass = generate_password(10,6,2,2)
+#       csv << [pass]
+#     end
+#   end
+# end
 
 
 
-#generate_password(10, 6, 2, 2)
+# #generate_password(10, 6, 2, 2)
 
-def check_password(filename)
-  pass = generate_password(10, 6, 2, 2)
-  #pass = "13H2&X+867"
-  csv_data = CSV.read(filename)
-  data_array=csv_data.flatten
-  if data_array.include?(pass)
-    puts 'Password Alreay Exist'
-  else
-    CSV.open(filename, 'a') do |csv|
-      csv << [pass]
-    end
-  end
-end
-#save_password('pass.csv')
-check_password('pass.csv')
+# def check_password(filename)
+#   pass = generate_password(10, 6, 2, 2)
+#   #pass = "13H2&X+867"
+#   csv_data = CSV.read(filename)
+#   data_array=csv_data.flatten
+#   if data_array.include?(pass)
+#     puts 'Password Alreay Exist'
+#   else
+#     CSV.open(filename, 'a') do |csv|
+#       csv << [pass]
+#     end
+#   end
+# end
+# #save_password('pass.csv')
+# check_password('pass.csv')
